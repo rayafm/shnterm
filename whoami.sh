4b822d3d3d56b26f22301d05a056be092e6fc610
rayafm <juniariprs360@gmail.com>
2024-12-25 16:11:58 +0700

    Add who am I

---
+++ whoami.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+name="Juniar Indah Purnama Raya Situmorang"
+food="toast"
+drink="cappucino coffee"
+echo -e "Hi!" "\nMy name is" $name"." "\nMy favorite food is" $food"." "\nMy favorite drink is" $drink"."
