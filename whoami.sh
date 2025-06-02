3aa0d51fbdaeb23ed47eb14911afb78b245f5d39
rayafm <juniariprs360@gmail.com>
2025-06-02 23:20:43 +0700

    Add Chapter 1 lesson

---
+++ whoami.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+name="Juniar Indah Purnama Raya Situmorang"
+food="toast"
+drink="cappuccino coffee"
+hobbies="reading, listening to music, singing"
+echo -e "Hi!"
+echo -e "My name is" $name
+echo -e "My favorite food is" $food
+echo -e "My favorite drink is" $drink
+echo -e "My hobbies are" ${hobbies[@]}
+echo "Nice to meet you!"
